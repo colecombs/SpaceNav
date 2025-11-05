c80b76ed74357eb75a9c022c3610e55f81c7c0dd
colecombs <cole.b.combs@gmail.com>
2025-11-05 03:17:25 +0000

    second try to upload test programs

---
+++ kernels.mk
@@ -0,0 +1,17 @@
+\begindata
+
+   KERNELS_TO_LOAD = (
+      'kernels/naif0012.tls'
+      'kernels/de440.bsp'
+      'kernels/gm_de440.tpc'
+      'kernels/pck00010.tpc'
+      'kernels/pck00011.tpc'
+      'kernels/naif0011.tls'
+      'kernels/L1_de441.bsp'
+      'kernels/L2_de441.bsp'
+      'kernels/L4_de441.bsp'
+      'kernels/L5_de441.bsp'
+   )
+
+\begintext
+
